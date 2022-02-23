e765b67edcc7624f4d8822e29d261ee95c0bcae0
tejasMetricoidtech <tejas@metricoidtech.com>
2022-02-23 23:19:19 +0530

    commit

---
+++ client/src/fireBase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/app"
+import 'firebase/auth'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBiEArx36uwah9ZOmM6-H3XqSTQohFOgpE",
+    authDomain: "task-pro-services.firebaseapp.com",
+    projectId: "task-pro-services",
+    storageBucket: "task-pro-services.appspot.com",
+    messagingSenderId: "374450768437",
+    appId: "1:374450768437:web:6cb5f76fc2d7178b3abf7c"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const googleAuthProvider = new firebase.auth.googleAuthProvider();
+
+export {auth, googleAuthProvider, facbookAuthProvider}
