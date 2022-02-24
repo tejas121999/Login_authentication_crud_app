cae4361e74c0fa59d668c58efe99fac5f9c202e2
tejasMetricoidtech <tejas@metricoidtech.com>
2022-02-24 22:39:53 +0530

    commit

---
+++ backend/models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    street: {
+        type: String,
+        require: true,
+        unique: true
+    }
+});
+
+module.exports = {
+    Post: mongoose.model('posts', PostSchema),
+};
